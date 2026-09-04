85723e72e4b9adafa3a38194d4b5254957586d4b
Damian Ruttan <ruttandamian@gmail.com>
2026-09-03 22:55:36 -0400

    feat: add application startup entry point

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-export {}
+import { buildApp } from './app'
+import { loadConfig } from './config'
+
+async function start(): Promise<void> {
+  const config = loadConfig()
+  const app = buildApp(config)
+  await app.listen({ port: config.port, host: config.host })
+}
+
+start().catch((error: unknown) => {
+  console.error(error)
+  process.exit(1)
+})
